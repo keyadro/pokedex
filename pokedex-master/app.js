ca0cd50bdc0d4c116f64f97f0b239da13575e58a
Mateus Alves de oliveira <54225530+keyadro@users.noreply.github.com>
2022-02-06 21:59:52 -0300

    Add files via upload

---
+++ pokedex-master/app.js
@@ -0,0 +1,34 @@
+const fetchPokemon = () => {
+    const getPokemonUrl = id => `https://pokeapi.co/api/v2/pokemon/${id}`
+
+    const pokemonPromises = []
+
+    for (let i = 1; i <= 150; i++) {
+        pokemonPromises.push(fetch(getPokemonUrl(i)).then(Response => Response.json()))
+        
+    }
+    Promise.all(pokemonPromises)
+        .then(pokemons => {
+
+            const listPokemons = pokemons.reduce((accumulator, pokemon) => {
+                const types = pokemon.types.map(typeInfor => typeInfor.type.name)
+           
+
+                accumulator += `
+            <li class="card ${types[0]} ">
+                <img class=" card-img"  alt ="${pokemon.name}" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png"/>
+                <h2 class="card-title"> ${pokemon.id}. ${pokemon.name}  </h2>
+                <p class ="card-subtitle">${types.join(" | ")}</p>
+        
+            </li>`
+                return accumulator
+            }, '')
+
+            const ul = document.querySelector('[data-js="pokedex"]')
+
+            ul.innerHTML = listPokemons
+        })
+}
+
+
+fetchPokemon()
